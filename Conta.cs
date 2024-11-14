69ffaa974e7fdc5d05dd679db1710a0d5c57a3d6
Alan Soares <darkalantm@gmail.com>
2024-11-13 20:22:51 -0400

    Exception

--- Conta.cs
+++ Conta.cs
@@ -4,14 +4,18 @@ public class Conta {
 
     public void setSaldo(double saldo)
     {
-
+        try { 
         if (saldo >= 0)
         {
             _saldo = saldo;
         }
-        else
+        else /*Lança uma Exessão*/
         {
-            WriteLine("Saldo Invalído");
+            throw new Exception("O Saldo não pode ser negativo!");
+        }
+        }catch(Exception ex) /*Lança uma Exessão de forma geral para todo tipo de erro*/
+        {
+            throw new Exception(ex.Message);
         }
     }
 };
--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
 ﻿using static System.Console;
 
-Conta c = new Conta();
-WriteLine("informe o saldo: ");
-double saldo = double.Parse(ReadLine());
+//Conta c = new Conta();
+//WriteLine("informe o saldo: ");
+//double saldo = double.Parse(ReadLine());
 
-c.setSaldo(saldo);
+//c.setSaldo(saldo);
+//try
+//{
+//    WriteLine("Digite um valor");
+//    int valor = Convert.ToInt32(ReadLine());
+
+//    WriteLine(valor / 0);
+//}
+//catch (Exception ex){
+//    WriteLine("Valor informado inválido!"+ex.Message);
+//}
+
+try {
+    Conta c = new Conta();
+    c.setSaldo(-60);
+}
+catch(Exception ex) {
+    WriteLine(ex.Message);
+}
